cc8d782dc5f380dc20c0418502e2d0103967338c
i8abyte <imransalim08@gmail.com>
2025-09-03 13:02:23 +1200

    test: fix the behaviour of the tests by removing the res object that does not exist in a Jest test environment

--- tests/routes/recipe.test.js
+++ tests/routes/recipe.test.js
@@ -9,8 +9,7 @@ test('there are recipes', () => {
   db.all('SELECT * FROM recipe', (err, rows) => {
     if (err) {
       console.error(err.message);
-      res.status(500);
-      res.send("500 Internal Server Error")
+      fail(err);
       return;
     }
     expect(rows).toEqual(data);
@@ -74,8 +73,7 @@ test('there are ingredients for each recipe', () => {
   db.all('SELECT * FROM ingredient', (err, rows) => {
     if (err) {
       console.error(err.message);
-      res.status(500);
-      res.send("500 Internal Server Error")
+      fail(err);
       return;
     }
     expect(rows).toEqual(data);
@@ -94,7 +92,7 @@ test('there are instructions for each recipe', () => {
       id: 2,
       recipe_name: 'Soft Boiled Eggs',
       instruction_number: 2,
-      instruction_description: "Once boiling, add an egg (or however many you'd like as long as they are in a single layer in a bottom of the pot), straight from the refridgerator into the pot. Replace the lid and let it continue to boil for exactly six minutes."
+      instruction_description: "Once boiling, add an egg (or however many you'd like as long as they are in a single layer in a bottom of the pot), straight from the refrigerator into the pot. Replace the lid and let it continue to boil for exactly six minutes."
     },
     {
       id: 3,
@@ -112,7 +110,7 @@ test('there are instructions for each recipe', () => {
       id: 5,
       recipe_name: 'Air Fryer Chicken Wings',
       instruction_number: 2,
-      instruction_description: 'Mix 1 teaspoon of salt with 1 teaspoon of baking powder per pound of chicken. Toss the dried chicken wings in this mixture until evenly coated. Place the wings on a rack set over a sheet pan and refridgerate for at least one hour.'
+      instruction_description: 'Mix 1 teaspoon of salt with 1 teaspoon of baking powder per pound of chicken. Toss the dried chicken wings in this mixture until evenly coated. Place the wings on a rack set over a sheet pan and refrigerate for at least one hour.'
     },
     {
       id: 6,
@@ -137,8 +135,7 @@ test('there are instructions for each recipe', () => {
   db.all('SELECT * FROM instruction', (err, rows) => {
     if (err) {
       console.error(err.message);
-      res.status(500);
-      res.send("500 Internal Server Error")
+      fail(err);
       return;
     }
     expect(rows).toEqual(data);
@@ -151,7 +148,7 @@ test('the recipe data is formatted correctly to be sent in the response body', (
       ingredients: [ 'large egg(s), chilled' ],
       instructions: {
         '1': 'Add 1 inch of water to a sauce pot, cover, and bring to a boil over high heat.',
-        '2': "Once boiling, add an egg (or however many you'd like as long as they are in a single layer in a bottom of the pot), straight from the refridgerator into the pot. Replace the lid and let it continue to boil for exactly six minutes.",
+        '2': "Once boiling, add an egg (or however many you'd like as long as they are in a single layer in a bottom of the pot), straight from the refrigerator into the pot. Replace the lid and let it continue to boil for exactly six minutes.",
         '3': 'After six minutes, remove the egg(s) from the pot and place them in an ice water bath or run under cool water until they are cool enough to handle. Peel, and enjoy!'
       }
     },
@@ -169,7 +166,7 @@ test('the recipe data is formatted correctly to be sent in the response body', (
       ],
       instructions: {
         '1': 'Pat the chicken wings dry with a paper towel. Make sure to get as much moisture off the skin as possible.',
-        '2': 'Mix 1 teaspoon of salt with 1 teaspoon of baking powder per pound of chicken. Toss the dried chicken wings in this mixture until evenly coated. Place the wings on a rack set over a sheet pan and refridgerate for at least one hour.',
+        '2': 'Mix 1 teaspoon of salt with 1 teaspoon of baking powder per pound of chicken. Toss the dried chicken wings in this mixture until evenly coated. Place the wings on a rack set over a sheet pan and refrigerate for at least one hour.',
         '3': 'Remove the wings from the fridge and pat them dry a second time. Mix the cornstarch, garlic powder, smoked paprika, onion powder, and black pepper called for in the recipe for every pound of chicken. Toss the wings in this mixture until thoroughly coated.',
         '4': 'Grease the air fryer basket and preheat it to 300°F. Arrange the wings in a single layer, making sure they do not touch. Cook for 15 minutes.',
         '5': 'Flip the wings over, raise the temperature of the air fryer to 450°F, and cook for 10 minutes. Use a meat thermometer to make sure the wings have an internal temperature of 165°F before serving.'
@@ -180,8 +177,7 @@ test('the recipe data is formatted correctly to be sent in the response body', (
   db.all('SELECT * FROM recipe', (err, rows) => {
     if (err) {
       console.error(err.message);
-      res.status(500);
-      res.send("500 Internal Server Error")
+      fail(err);
       return;
     }
     let recipes = rows;
@@ -189,8 +185,7 @@ test('the recipe data is formatted correctly to be sent in the response body', (
     db.all('SELECT * FROM ingredient', (err, rows) => {
       if (err) {
         console.error(err.message);
-        res.status(500);
-        res.send("500 Internal Server Error")
+        fail(err);
         return;
       }
       let ingredients = rows;
@@ -198,8 +193,7 @@ test('the recipe data is formatted correctly to be sent in the response body', (
       db.all('SELECT * FROM instruction', (err, rows) => {
         if (err) {
             console.error(err.message);
-            res.status(500);
-            res.send("500 Internal Server Error")
+            fail(err);
             return;
         }
         let instructions = rows;
